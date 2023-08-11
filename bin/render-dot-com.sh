b7c26ff638673cb88e3f17bac842d461fa845e72
Olaf Alders <olaf@wundersolutions.com>
2023-08-11 19:04:06 -0400

    Add a build script for render.com
    This should allow us to pin the Hugo version used for site previews

---
+++ bin/render-dot-com.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e -u -x -o pipefail
+
+# Stolen from https://community.render.com/t/how-to-define-hugo-version/390/8
+# In your Render dashboard, set the HUGO_VERSION env var for your static site. You'll get the same experience as, say, Netlify.
+
+TAR_NAME="hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz"
+
+hugo version # Output the OLD version
+if [[ ! -f $XDG_CACHE_HOME/hugo ]]; then
+    echo "...Downloading HUGO"
+    mkdir -p ~/tmp
+    wget -P ~/tmp https://github.com/gohugoio/hugo/releases/download/v"$HUGO_VERSION/$TAR_NAME"
+    cd ~/tmp || exit 1
+    echo "...Extracting HUGO"
+    tar -xzvf "$TAR_NAME"
+    echo "...Moving HUGO"
+    mv hugo "$XDG_CACHE_HOME/hugo"
+    # Make sure we return to where we were.
+    cd "$HOME/project/src"
+else
+    echo "...Using HUGO from build cache"
+fi
+
+"$XDG_CACHE_HOME/hugo" version
+
+# Render sets IS_PULL_REQUEST to true for PR previews.
+if [ "${IS_PULL_REQUEST:-}" = "true" ]; then
+    $XDG_CACHE_HOME/hugo --gc -e preview
+else
+    $XDG_CACHE_HOME/hugo --gc --minify
+fi
